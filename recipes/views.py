420549fc1326170fb5adc2050bcc3e0acf8e68e7
Sara Fernandez <saradfrz@gmail.com>
2022-11-23 20:36:56 -0300

    aula 24

--- recipes/views.py
+++ recipes/views.py
@@ -1,9 +1,9 @@
-# from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def home(request):
-    return HttpResponse('Home')
+    return render(request, 'home.html')
 
 
 def contato(request):
